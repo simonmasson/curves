fa1bdbec2d64aff347eb3c03b63375de83275f2f
Simon Masson <simon.masson@yahoo.fr>
2022-02-14 10:17:30 -0400

    remove tests for fake curves

--- vesta/src/curves/tests.rs
+++ vesta/src/curves/tests.rs
@@ -17,23 +17,23 @@ use ark_algebra_test_templates::{
     groups::group_test,
 };
 
-#[test]
-fn test_projective_curve() {
-    curve_tests::<Projective>();
-    sw_tests::<VestaParameters>();
-}
+// #[test]
+// fn test_projective_curve() {
+//     curve_tests::<Projective>();
+//     sw_tests::<VestaParameters>();
+// }
 
-#[test]
-fn test_projective_group() {
-    let mut rng = test_rng();
-    let a: Projective = rng.gen();
-    let b: Projective = rng.gen();
-    group_test(a, b);
-}
+// #[test]
+// fn test_projective_group() {
+//     let mut rng = test_rng();
+//     let a: Projective = rng.gen();
+//     let b: Projective = rng.gen();
+//     group_test(a, b);
+// }
 
-#[test]
-fn test_generator() {
-    let generator = Affine::prime_subgroup_generator();
-    assert!(generator.is_on_curve());
-    assert!(generator.is_in_correct_subgroup_assuming_on_curve());
-}
+// #[test]
+// fn test_generator() {
+//     let generator = Affine::prime_subgroup_generator();
+//     assert!(generator.is_on_curve());
+//     assert!(generator.is_in_correct_subgroup_assuming_on_curve());
+// }
